55d36a8097d096fa11c4708219aca929172cc73d
Hendrik Jonas Meyer <t4cc0re@gmail.com>
2014-03-20 16:48:25 +0100

    Big update pack!
    * Fixed endlines to "\n"
    * Expanded example page
    + Added ::verifyOJSL()
    * Customized CSS and colors
    
    Todo:
    
    - Write OJSL-Specs
    - fix reported (or otherwhise found) bugs

--- OpenJSLog.js
+++ OpenJSLog.js
@@ -1,1 +1,108 @@
-/** OpenJSLog Copyright (C) 2014 Hendrik 'Xendo' Meyer This file is part of OpenJSLog OpenJSLog is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. OpenJSLog is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with Foobar. If not, see <http://www.gnu.org/licenses/>. *//** * This is the Object you want to stringify to analyze it with the Analyzer. * @type {{data: Array, timestamps: Array}} */var LogBuffer = {data: [], timestamps: []};/** * Specifies the Devmode setting. * @type {boolean} */OJSLdevmode = (OJSLgetCookie("OJSLdevmode") == "1");/** * OpenJSLog: Logs the given input with a timestamp * * @param log               The Object to log. * @param force             Whether or not to force a log to console. * @param spit              Output the Logbuffer to the Console * @param emptyAfterSpit    Wether or not to empty the Logbuffer after outputting it. */function Log(log, force, spit, emptyAfterSpit){    //Force a log to console if the devmode is set    //or the 'force'-parameter has been set to true.    var directlog = (OJSLdevmode) ? true : force;    if (directlog) {        console.log(log);    }    //Create a timestamp (milliseconds since 1970-01-01, 00:00:00 UTC)    var timestamp = new Date().getTime();    //Push Data and timestamp into the LogBufer.    LogBuffer.data.push(log);    LogBuffer.timestamps.push(timestamp);    //If  'spit' is set to true output the LogBuffer to the    //console and if 'emptyAfterSpit' is also true delete/clear    //the buffer afterwards    if (spit === true) {        for(var index in LogBuffer){            console.log(LogBuffer.data[index]);        }        if(emptyAfterSpit === true){            LogBuffer = {data: [], timestamps: []};        }    }}/** * OJSLsetCookie: Set a cookie * @param cname     The name the cookie is saved as * @param cvalue    The value of the cookie * @param exdays    The expirationdate in days(!) */function OJSLsetCookie(cname,cvalue,exdays){    var d = new Date();    d.setTime(d.getTime()+(exdays*24*60*60*1000));    var expires = "expires="+d.toGMTString();    document.cookie = cname + "=" + cvalue + "; " + expires;}/** * OJSLgetCookie: Gets a cookie * @param cname The name of the cookie to get * @returns {string}    The value of the cookie */function OJSLgetCookie(cname){    var name = cname + "=";    var ca = document.cookie.split(';');    for(var i=0; i<ca.length; i++)    {        var c = ca[i].trim();        if (c.indexOf(name)==0) return c.substring(name.length,c.length);    }    return "";}/** * setDevmode: Sets the devmode to either true or false * @param value */function OJSLsetDevmode(value){    if(value === true){        OJSLsetCookie("OJSLdevmode","1",36500);        OJSLdevmode = true;    } else {        OJSLsetCookie("OJSLdevmode","0",36500);        OJSLdevmode = false;    }}
+/**
+ OpenJSLog
+ Copyright (C) 2014 Hendrik 'Xendo' Meyer
+
+ This file is part of OpenJSLog
+
+ OpenJSLog is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 2 of the License, or
+ (at your option) any later version.
+
+ OpenJSLog is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Foobar. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * This is the Object you want to stringify to analyze it with the Analyzer.
+ * @type {{data: Array, timestamps: Array}}
+ */
+var LogBuffer = {data: [], timestamps: []};
+
+/**
+ * Specifies the Devmode setting.
+ * @type {boolean}
+ */
+OJSLdevmode = (OJSLgetCookie("OJSLdevmode") == "1");
+
+/**
+ * OpenJSLog: Logs the given input with a timestamp
+ *
+ * @param log               The Object to log.
+ * @param force             Whether or not to force a log to console.
+ * @param spit              Output the Logbuffer to the Console
+ * @param emptyAfterSpit    Wether or not to empty the Logbuffer after outputting it.
+ */
+function Log(log, force, spit, emptyAfterSpit) {
+    //Force a log to console if the devmode is set
+    //or the 'force'-parameter has been set to true.
+    var directlog = (OJSLdevmode) ? true : force;
+    if (directlog) {
+        console.log(log);
+    }
+
+    //Create a timestamp (milliseconds since 1970-01-01, 00:00:00 UTC)
+    var timestamp = new Date().getTime();
+
+    //Push Data and timestamp into the LogBufer.
+    LogBuffer.data.push(log);
+    LogBuffer.timestamps.push(timestamp);
+
+    //If  'spit' is set to true output the LogBuffer to the
+    //console and if 'emptyAfterSpit' is also true delete/clear
+    //the buffer afterwards
+    if (spit === true) {
+        for (var index in LogBuffer) {
+            console.log(LogBuffer.data[index]);
+        }
+        if (emptyAfterSpit === true) {
+            LogBuffer = {data: [], timestamps: []};
+        }
+    }
+}
+
+/**
+ * OJSLsetCookie: Set a cookie
+ * @param cname     The name the cookie is saved as
+ * @param cvalue    The value of the cookie
+ * @param exdays    The expirationdate in days(!)
+ */
+function OJSLsetCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+/**
+ * OJSLgetCookie: Gets a cookie
+ * @param cname The name of the cookie to get
+ * @returns {string}    The value of the cookie
+ */
+function OJSLgetCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+/**
+ * setDevmode: Sets the devmode to either true or false
+ * @param value
+ */
+function OJSLsetDevmode(value) {
+    if (value === true) {
+        OJSLsetCookie("OJSLdevmode", "1", 36500);
+        OJSLdevmode = true;
+    } else {
+        OJSLsetCookie("OJSLdevmode", "0", 36500);
+        OJSLdevmode = false;
+    }
+}
--- OpenJSLogAnalyzer.js
+++ OpenJSLogAnalyzer.js
@@ -25,7 +25,7 @@
  * @returns {string}    The output
  */
 function pad(num, size) {
-    var s = num+"";
+    var s = num + "";
     while (s.length < size) s = "0" + s;
     return s;
 }
@@ -37,7 +37,7 @@ function pad(num, size) {
  */
 function buildList(parentObject, parentElement) {
     var rEl, rParent;
-    
+
     //generate a new unordered list to hold it's children
     rParent = document.createElement('ul');
 
@@ -49,17 +49,22 @@ function buildList(parentObject, parentElement) {
         //different to map it's possible children aswell
         if (typeof parentObject[key] == "object") {
             //If it is an Array set true, otherwhise false
-            isArray = parentObject[key] instanceof Array;
+            var isArray = parentObject[key] instanceof Array;
             rEl.className = isArray ? 'parent array' : 'parent'; //Set up some styling
             //Prefix with the Type of Object (Object|Array) and it's corresponding indicator ({|[)
             //'key' is the key of the Object. If it is a direct child of 'data' this will get replaced with the timestamp.
-            rEl.innerHTML = '<span class="key">' + key +'</span>: ' + ((isArray === true) ? ('[Array] ['): ('[Object] {'));
+            rEl.innerHTML = '<span class="key">' + key + '</span>: ' + ((isArray === true) ? ('[Array] [') : ('[Object] {'));
             //Do all the same stuff again for the Object's children. Recursion FTW!
             buildList(parentObject[key], rEl);
+            //Add the closing parentheses:
+            closingparantheses = (document.createElement('li'));
+            closingparantheses.className = "closing " + (isArray ? 'array' : 'object');
+            closingparantheses.innerHTML = isArray ? ']' : '}';
+            rEl.appendChild(closingparantheses);
         } else {
             //Prefix with the Type (e.g. "[String]")
             //'key' is the key of the Object. If it is a direct child of 'data' this will get replaced with the timestamp.
-            rEl.innerHTML = '<span class="key">' + key +'</span>: [' + typeof parentObject[key] + '] <span class="value">' + parentObject[key] + '</span>';
+            rEl.innerHTML = '<span class="key">' + key + '</span>: [' + typeof parentObject[key] + '] <span class="value">' + parentObject[key] + '</span>';
         }
         //add the lowercase type as a css-class to be able to style each type individually.
         rEl.className += " " + (typeof parentObject[key]).toLowerCase();
@@ -75,12 +80,19 @@ function buildList(parentObject, parentElement) {
  * OJSLanalyze: Builds the complete minimizeable List for a browsable LogBuffer.
  * @param object    The (parsed JSON) Object to Analyze. (Can also be the LogBuffer directly)
  * @param element   The element to host the container in.
- * @constructor
  */
 function OJSLanalyze(object, element) {
-    //Make sure the container is empty and able to be styled.
-    element.innerHTML = '';
-    element.className = "loganalyzer";
+
+    if (!verifyOJSL(object)) {
+        //If the given object is not valid display an error.
+        element.innerHTML = 'Invalid OpenJSLog!';
+        element.className = 'loganalyzer notLoaded';
+        return;
+    } else {
+        //Make sure the container is empty and able to be styled.
+        element.innerHTML = '';
+        element.className = "loganalyzer";
+    }
 
     //Add listeners to the minizizeable objects.
     element.addEventListener('click', function (e) {
@@ -97,16 +109,55 @@ function OJSLanalyze(object, element) {
     buildList(object.data, element);
     //Weired for loop, but it does not work otherwhise.. Hints are welcome!
     var nodeindex = 0;
-    for(var notUsed in element.childNodes[0].childNodes){
-        try{
+    for (var notUsed in element.childNodes[0].childNodes) {
+        try {
             //Setup the timestamp for the current node/element.
-            date = new Date(Number(object.timestamps[nodeindex]));
-            title = date.getFullYear() + "-" + pad(date.getMonth(),2) + "-" + pad(date.getDate(),2) + " " + pad(date. getHours(),2) + ":" + pad(date.getMinutes(),2) + ":" + pad(date.getSeconds(),2) + "." + pad(date.getMilliseconds(),3);
+            var date = new Date(Number(object.timestamps[nodeindex]));
+            var title = "";
+            //The timestamp can only be built if the date-elements are Numbers.
+            if (isNaN(date.getFullYear())) {
+                title = "no timestamp provided";
+            } else {
+                title = date.getFullYear() + "-" + pad(date.getMonth(), 2) + "-" + pad(date.getDate(), 2) + " " + pad(date.getHours(), 2) + ":" + pad(date.getMinutes(), 2) + ":" + pad(date.getSeconds(), 2) + "." + pad(date.getMilliseconds(), 3);
+            }
             //This weired thing is the path to the "title" of the element in the DOM.
             //Just set the .innerHTML and we are golden :)
             element.childNodes[0].childNodes[nodeindex].childNodes[0].innerHTML = title;
             //Finally increase the counter for the next iteration
             nodeindex++;
-        }catch (err){}
+        } catch (err) {
+        }
+    }
+}
+
+//Specs: data is mandatory, timestamp can be omitted.
+
+/**
+ * verifyOJSL: Verifies the compliance of an object or JSON-encoded string
+ * with OJSL specifications
+ *
+ * Specs: data is mandatory, timestamp may be omitted.
+ *
+ * @param input JSON String
+ * @returns {boolean} Wherther or not it matches the OpenJSLog specs.
+ */
+function verifyOJSL(input, isJSON) {
+    try {
+        //If input is Json then parse it first
+        var tmp = isJSON ? JSON.parse(input) : input;
+        //If  the object contains the Data-portion it is fine!
+        if (tmp.data != null) {
+            tmp = {};
+            return true;
+        } else {
+            //if not throw an error.
+            tmp = {};
+            throw ReferenceError;
+        }
+    } catch (err) {
+        //This catches the errors where the data-portion is
+        // not found and where the JSON could not be parsed
+        tmp = {};
+        return false;
     }
 }
